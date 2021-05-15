dac2a8018db94798e586606132fbc6f7da682f4e
huydaodang <huydaodang2706@gmail.com>
2021-05-16 00:33:46 +0700

    Use firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAGsBA_Rw0d3UjgTc9zygLUgHpAUNlEa8c",
+    authDomain: "fb-firebase-9afa2.firebaseapp.com",
+    projectId: "fb-firebase-9afa2",
+    storageBucket: "fb-firebase-9afa2.appspot.com",
+    messagingSenderId: "647594656548",
+    appId: "1:647594656548:web:0917e4d0b7d4d3f668e9b1"
+};
+
+firebase.initializeApp(firebaseConfig);
