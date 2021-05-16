fdfab8bf4ff8c6265924494af35c309c2670c29f
huydd <nuhoangchenhac@gmail.com>
2021-05-16 15:25:00 +0700

    Login by Authentication

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,35 +1,31 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
-
-var firebaseConfig = {
+if (!firebase.apps.length) {
+  var firebaseConfig = {
     apiKey: "AIzaSyAGsBA_Rw0d3UjgTc9zygLUgHpAUNlEa8c",
     authDomain: "fb-firebase-9afa2.firebaseapp.com",
     projectId: "fb-firebase-9afa2",
     storageBucket: "fb-firebase-9afa2.appspot.com",
     messagingSenderId: "647594656548",
-    appId: "1:647594656548:web:0917e4d0b7d4d3f668e9b1"
-};
-
-firebase.initializeApp(firebaseConfig);
-
+    appId: "1:647594656548:web:0917e4d0b7d4d3f668e9b1",
+  };
+  firebase.initializeApp(firebaseConfig);
+}
 
 const db = firebase.firestore();
+export const auth = firebase.auth();
 export default firebase;
 
 export const getFirebaseItems = async () => {
   try {
-    const snapshot = await db
-      .collection("todos")
-      .get();
-    const items = snapshot.docs.map(
-      (doc) => ({ ...doc.data(), id: doc.id })
-    );
+    const snapshot = await db.collection("todos").get();
+    const items = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
     return items;
   } catch (err) {
     console.log(err);
     return [];
   }
-}
+};
 
 export const addFirebaseItem = async (item) => {
   try {
@@ -38,7 +34,7 @@ export const addFirebaseItem = async (item) => {
   } catch (err) {
     console.log(err);
   }
-}
+};
 
 export const updateFirebaseItem = async (item, id) => {
   try {
@@ -47,13 +43,68 @@ export const updateFirebaseItem = async (item, id) => {
   } catch (err) {
     console.log(err);
   }
-}
+};
 
 export const clearFirebaseItem = async (item) => {
   const todoRef = db.collection("todos").doc(item.id);
-  await todoRef.delete().then(function () {
-  }).catch(function (err) {
-    console.log(err);
-  });
+  await todoRef
+    .delete()
+    .then(function () {})
+    .catch(function (err) {
+      console.log(err);
+    });
 };
 
+export const uiConfig = {
+  signInFlow: "popup",
+  signInSuccessUrl: "/",
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+};
+
+export const storeUserInfo = async (user) => {
+  const { uid } = user;
+  const userDoc = await db.collection("users").doc(uid).get();
+  if (!userDoc.exists) {
+    await db.collection("users").doc(uid).set({ name: user.displayName });
+    return {
+      name: user.displayName,
+      id: uid,
+    };
+  } else {
+    return {
+      id: uid,
+      ...userDoc.data(),
+    };
+  }
+};
+
+export const updateUser = async (user, image) => {
+  try {
+    const userDoc = await firebase
+      .firestore()
+      .collection("users")
+      .doc(user.id)
+      .get();
+    if (userDoc.exists) {
+      await firebase
+        .firestore()
+        .collection("users")
+        .doc(user.id)
+        .update({ ...userDoc.data(), image: image });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const uploadImage = async (image) => {
+  const ref = firebase.storage().ref().child(`/images/${image.name}`);
+  let downloadUrl = "";
+  try {
+    await ref.put(image);
+    downloadUrl = await ref.getDownloadURL();
+  } catch (err) {
+    console.log(err);
+  }
+  return downloadUrl;
+};
